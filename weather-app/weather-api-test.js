95fef24c4b599998831f3d0637afe98a3e431585
Amresh Sinha <amarvijay62@outlook.com>
2024-05-01 21:29:24 +0530

    first commit

---
+++ weather-app/weather-api-test.js
@@ -0,0 +1,27 @@
+import { WeatherAPI } from "./weather-api-test.js";
+
+function testConstructURL() {
+  const weatherAPIObj = new WeatherAPI();
+
+  const weatherAPIURL = weatherAPIObj.constructURL("France");
+
+  console.log(weatherAPIURL);
+}
+
+async function testInvokeURL() {
+  const weatherAPIObj = new WeatherAPI();
+
+  const weatherURL = weatherAPIObj.constructURL("France");
+  console.log(weatherURL);
+
+  const weatherJSON = await weatherAPIObj.invokeWeatherURL();
+
+  console.log(weatherJSON);
+
+  // call invokeURL()
+  // print(json_response)
+}
+
+// testConstructURL();
+
+testInvokeURL();
